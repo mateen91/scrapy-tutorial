bc2296914a542a4fe5a56b45a1fd584883a9f57f
Mateen Ahmed <mateen.ahmed@arbisoft.com>
2017-12-13 18:18:59 +0500

    user_agent and download delay added

--- stackoverflow_jobs/stackoverflow_jobs/settings.py
+++ stackoverflow_jobs/stackoverflow_jobs/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'stackoverflow_jobs.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'stackoverflow_jobs (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
